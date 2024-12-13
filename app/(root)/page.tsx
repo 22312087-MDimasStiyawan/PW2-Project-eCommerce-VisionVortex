ecc8b3f62ef6fc7680bb11d2c04fbcc8ad625d51
Mdims75 <stiyawandims@gmail.com>
2024-12-14 03:32:21 +0700

    environment setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+// import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <button className="btn btn-success">Success</button>
+    </div>
+  );
+}
